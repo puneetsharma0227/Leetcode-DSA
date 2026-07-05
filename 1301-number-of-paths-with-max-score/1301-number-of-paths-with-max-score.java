cdc0b2f8358c7bbb5c761c4983ccc7df4779c125
puneetsharma0227 <puneetsharma0227@gmail.com>
2026-07-05 22:04:09 +0530

    Time: 12 ms (77.65%), Space: 47.1 MB (28.24%) - LeetHub

---
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
@@ -0,0 +1,74 @@
+class Solution {
+
+    static final int MOD = 1_000_000_007;
+
+    public int[] pathsWithMaxScore(List<String> board) {
+
+        int n = board.size();
+
+        int[][] score = new int[n][n];
+        int[][] ways = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(score[i], -1);
+        }
+
+        score[0][0] = 0;
+        ways[0][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                char c = board.get(i).charAt(j);
+
+                if (c == 'X') continue;
+
+                if (i == 0 && j == 0) continue;
+
+                int best = -1;
+                int cnt = 0;
+
+                int[][] dir = {{-1,0},{0,-1},{-1,-1}};
+
+                for (int[] d : dir) {
+
+                    int x = i + d[0];
+                    int y = j + d[1];
+
+                    if (x < 0 || y < 0) continue;
+
+                    if (score[x][y] == -1) continue;
+
+                    if (score[x][y] > best) {
+
+                        best = score[x][y];
+                        cnt = ways[x][y];
+
+                    } else if (score[x][y] == best) {
+
+                        cnt = (cnt + ways[x][y]) % MOD;
+                    }
+                }
+
+                if (best == -1) continue;
+
+                int val = 0;
+
+                if (Character.isDigit(c))
+                    val = c - '0';
+
+                score[i][j] = best + val;
+                ways[i][j] = cnt;
+            }
+        }
+
+        if (ways[n-1][n-1] == 0)
+            return new int[]{0,0};
+
+        return new int[]{
+                score[n-1][n-1],
+                ways[n-1][n-1]
+        };
+    }
+}
